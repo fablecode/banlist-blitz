a08816521a535927cad2de2692956cb821665d7f
fablecode <fablecode@users.noreply.github.com>
2024-08-11 11:20:11 +0200

    Format and Traditional exceptions

---
+++ src/BanlistBlitz/Exceptions/TcgAdvancedFormatException.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace BanlistBlitz.Exceptions;
+
+public class TcgAdvancedFormatException : Exception
+{
+    public DataRow Card { get; }
+
+    public TcgAdvancedFormatException(DataRow card)
+    {
+        Card = card;
+    }
+}
---
+++ src/BanlistBlitz/Exceptions/TcgAdvancedTraditionalException.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace BanlistBlitz.Exceptions;
+
+public class TcgAdvancedTraditionalException : Exception
+{
+    public DataRow Card { get; }
+
+    public TcgAdvancedTraditionalException(DataRow card)
+    {
+        Card = card;
+    }
+}
